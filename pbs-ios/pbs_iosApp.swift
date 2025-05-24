6960eb7efee1fa039c7c62199e97b40ed04570bc
Jesper Dinger <dev@jesperdinger.com>
2025-05-25 00:10:51 +0200

    Initial Commit

---
+++ pbs-ios/pbs_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  pbs_iosApp.swift
+//  pbs-ios
+//
+//  Created by Jesper Dinger on 25/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct pbs_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
