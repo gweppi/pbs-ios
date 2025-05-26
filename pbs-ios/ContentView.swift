ebc6bd2270d4adde68870d53cc08f12583addabe
Jesper Dinger <dev@jesperdinger.com>
2025-05-26 12:02:33 +0200

    Only commited one file...

--- pbs-ios/ContentView.swift
+++ pbs-ios/ContentView.swift
@@ -8,14 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    
+    @State private var vm = VM()
+    @State private var athleteId: String = "5133523"
+    
+    @State private var data: Obj?
+    @State private var error: Error?
+    
+    @State private var isSearching = false
+    
+    init() {
+        UISearchBar.appearance().showsCancelButton = false
+    }
+    
+    func fetch(id: String = "5133523") async {
+//        Valid ids have a length of seven, this reduces the number of calls to the api
+        if id.count < 7 { return }
+        
+        do {
+            self.data = try await vm.fetch(id: id)
+        } catch {
+            self.error = error
         }
-        .padding()
+    }
+    
+    var body: some View {
+        NavigationView {
+            if data != nil {
+                List(data?.pbs ?? [], id: \.self) { pb in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(pb.event)
+                            HStack {
+                                Text(pb.time)
+                                    .bold()
+                                Text("|")
+                                Text(pb.course)
+                            }
+                        }
+                        
+                        Spacer()
+                        
+                        Text(pb.date)
+                    }
+                }
+                .navigationTitle("\(data?.info.lastName ?? ""), \(data?.info.firstName ?? "")")
+                .onTapGesture { isSearching = false }
+            } else {
+                if let error {
+                    if let pbError = error as? PBError {
+                        Text(pbError.error)
+                    } else {
+                        Text(error.localizedDescription)
+                    }
+                } else {
+                    ProgressView()
+                }
+            }
+        }
+        .searchable(text: $athleteId, isPresented: $isSearching)
+        .task {
+            await fetch()
+        }
+        .onChange(of: athleteId) { _, newValue in
+            Task { await fetch(id: newValue) }
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIResponder.keyboardDidHideNotification)) { _ in
+            isSearching = false
+        }
     }
 }
 
--- pbs-ios/vm.swift
+++ pbs-ios/vm.swift
@@ -6,3 +6,72 @@
 //
 
 import Foundation
+
+enum VMError: Error {
+    case urlWrong
+    case unknown
+    case baseUrlNotFound
+}
+
+@Observable
+class VM {
+    
+    func fetch(id: String) async throws -> Obj {
+        var url: URL?
+        #if targetEnvironment(simulator)
+            url = URL(string: "http://localhost:8080/?athleteId=\(id)")
+        #else
+//        Get url from Secrets.xcconfig
+            if let baseUrl = Bundle.main.object(forInfoDictionaryKey: "API_ENDPOINT") as? String {
+                url = URL(string: "https://\(baseUrl)/?athleteId=\(id)")
+            } else {
+                throw VMError.baseUrlNotFound
+            }
+        #endif
+        guard let url else { throw VMError.urlWrong }
+        
+        let request = URLRequest(url: url)
+        let (data, res) = try await URLSession.shared.data(for: request)
+        
+        if let httpRes = res as? HTTPURLResponse {
+            switch httpRes.statusCode {
+                case 200:
+                    let decoded = try JSONDecoder().decode(Obj.self, from: data)
+                    return decoded
+                case 400, 404:
+                    let decoded = try JSONDecoder().decode(PBError.self, from: data)
+                    throw decoded
+                default:
+                    throw VMError.unknown
+            }
+        }
+        
+        throw VMError.unknown
+    }
+}
+
+struct PBError: Codable, Error {
+    var error: String
+}
+
+struct Obj: Codable {
+    let info: AthleteInfo
+    let pbs: [PB]
+}
+
+struct PB: Codable, Hashable {
+    let event: String
+    let course: String
+    let time: String
+    let pts: String
+    let date: String
+    let city: String
+    let name: String
+}
+
+struct AthleteInfo: Codable {
+    let id: String
+    let firstName: String
+    let lastName: String
+    let dobYear: String
+}
